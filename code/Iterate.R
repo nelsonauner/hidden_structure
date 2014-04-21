6f5c958df22bba9ccbe09a90f89b330289ce1c9a
mataddy <mataddy@outlook.com>
2014-04-21 16:04:48 -0500

    office changes

--- code/Iterate.R
+++ code/Iterate.R
@@ -5,51 +5,69 @@
 # borrowing heavily from documentation: http://cran.r-project.org/web/packages/textir/textir.pdf
 # It would  be nice to implement some error checking
 
-iter_cluster <- function(y,clusters,X,n.loop,debug=FALSE,cl=NULL,collapse=FALSE) {
-  require('plyr');require(textir);ptm<-proc.time()
+iter_cluster <- function(
+  y,
+  clusters,
+  X,
+  n.loop,
+  debug=FALSE,
+  cl=NULL,
+  collapse=FALSE) 
+{
+  require('plyr')
+  require(textir)
+  ptm<-proc.time()
+  
   m <- rowSums(X)
-  # mnlm does not handle factors! Turn our factor (membership = 1,2 or 3) into vector (membership = [0 1 0]), etc
-  cl_matrix <- model.matrix(formula(~0+as.factor(clusters)))
+  # Turn our factor (membership = 1,2 or 3) 
+  # into vector (membership = [0 1 0]), etc
+  cl_matrix <- sparse.model.matrix(formula(~0+as.factor(clusters)))
   ncl <- dim(cl_matrix)[2]  #Keep track of how many clusters we're using. 
-  Y<-Y_orig <- as.matrix(cbind(y,cl_matrix))
+  Y<- Y_orig <- cBind(y,cl_matrix)
   d.Y <- dim(Y)[2]
   n.meta <- dim(as.matrix(y))[2]
-  cluster_likes<-full_likes <- rep(NA,n.loop) #store likelihood updates here! 
-  h.clusters <- array(,dim=c(dim(X)[1],n.loop)) #we'll keep track of cluster assignments over time here
+  #store likelihood updates here! 
+  cluster_likes<-full_likes <- rep(NA,n.loop) 
+  #we'll keep track of cluster assignments over time here
+  h.clusters <- array(,dim=c(dim(X)[1],n.loop)) 
   fits <- mnlm(cl,Y ,X, bins=5, gamma=1, nlambda=10); 
   B <- coef(fits)
 
-  res = list(NULL,clusters,NULL,NULL);names(res) <- c("likes","clusters","B","time")
-  cust_sweep <- function(m,v) { t(t(m)+v) }  #this adds vector v to every row in matrix m
+  res = list(NULL,clusters,NULL,NULL)
+  names(res) <- c("likes","clusters","B","time")
+  #we'll keep track of cluster assignments over time here
   #normalize <- function(Xhat,vector) log(rowSums(exp(cust_sweep(Xhat,x))))
   for (i in 1:n.loop) {
-	Gamma_cl <- B[(2+n.meta):d.Y+1,] #include intercept term...
-    ll_left <- X%*%t(Gamma_cl)   #This ignores the alpha and meta data terms of the coeffecient matrix 
+	  Gamma_cl <- B[(1+n.meta):d.Y+1,] #include intercept term...
+    #This ignores the alpha and meta data terms of the coeffecient matrix 
+    ll_left <- X%*%t(Gamma_cl)   
     #This is definitely necessary (above)
     if(debug) print("ll_left OK")
-    #The above formula verified to correctly multiply! We want to maximize ll_left
-    #  This operation could be simplified by collapsing over metadata levels:        #
-    Xhat <- cust_sweep(as.matrix(Y[,1:n.meta]) %*% B[2:(1+n.meta),] ,B[1,]) #add intercept and v_i*theta term
-    #The apply function is flipping our matrix, etf. 
-    #ll_penal_tot <- vapply(B[3:6,],MARGIN=1,FUN.VALUE=1,FUN=function(x) cust_sweep(ll_penal,x))
-    #let's try plyr version:
-    #A quick function to 
-	#Now, for each cluster coeffecient (row of Gamma_cl, add it to Xhat, then take exp,sum, and log
-    ll_right = apply(X=Gamma_cl,MARGIN=1,FUN=function(x,matrix){log(rowSums(exp( t(t(matrix)+x) )))},matrix=Xhat)
-    #system.time(apply(X=Gamma_cl,MARGIN=1,FUN=function(x,matrix){log(rowSums(exp( t(t(matrix)+x) )))},matrix=Xhat))
-    #system.time(parRapply(cl,x = Gamma_cl,FUN=function(x,matrix){log(rowSums(exp( t(t(matrix)+x) )))},matrix=Xhat))
+    # The above formula verified to correctly multiply! 
+    # We want to maximize ll_left + ll_right
+    # This operation could be simplified by collapsing over metadata levels.
+    VPHI <- Y[,1:n.meta,drop=FALSE] %*% B[2:(1+n.meta),,drop=FALSE]  
+    tXhat <- t(VPHI)+B[1,]
+	  # Now, for each cluster coeffecient 
+    # (row of Gamma_cl, add it to Xhat, then take exp,sum, and log
+    xhg <- function(j){
+      expetaj <- exp(tXhat +  Gamma_cl[j,])
+      log(colSums(expetaj))
+    } 
+    ll_right = do.call(cbind,lapply(1:ncl, xhg))
     if(debug) print("ll_right OK")
     #Compute total log likelihood
-    ll <- m*ll_right - ll_left  #We cannot expect to be positive as we took out some common terms.   
-    cluster_likes[i] <- sum(apply(ll,MARGIN=1,FUN=min))
+    ll <- m*ll_right - ll_left   
+    h.clusters[,i] <- 
+      n_cl <- factor(apply(ll,MARGIN=1,FUN=which.min),levels=1:ncl) #select cluster to minimize L   
+    cluster_likes[i] <- sum(apply(ll,MARGIN=1,FUN=min)) ## use n_cl
     full_likes[i] = cluster_likes[i] - sum(rowSums(Xhat*X)) #Add in the likelihood from the -x'(alpha+phi*vi)
     #Select new cluster membership if better. 
-    h.clusters[,i] <- n_cl <- factor(apply(ll,MARGIN=1,FUN=which.min),levels=1:(ncl-1)) #select cluster to minimize L 
-    n_cl_matrix <- model.matrix(formula(~0+(n_cl))) #and convert to [0 0 1] form. 
+    n_cl_matrix <- model.matrix(formula(~0+(n_cl))) # and convert to [0 0 1] form. 
     #update our Y:
-    Y[,(2+n.meta):d.Y] <- n_cl_matrix
+    Y[,(1+n.meta):d.Y] <- n_cl_matrix
     #And refit: 
-    fits <- mnlm(cl,Y ,X, bins=5, gamma=1, nlambda=10); B <- coef(fits)  #
+    fits <- mnlm(cl,Y ,X, bins=5, gamma=1, nlambda=10);
     B <- coef(fits) #Pull the coeffecients
   }
   res$time <- proc.time()-ptm
