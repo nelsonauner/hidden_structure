65d0261a0d3bc598a9b3f6c6c39d7e361da778f6
=Nelson Auner <ne.auner@gmail.com>
2014-04-25 23:12:48 -0500

    more references

--- code/Cluster_congress.R
+++ code/Cluster_congress.R
@@ -31,9 +31,10 @@ make_cl3 <- function(i) {kmeans(resids,i)$cluster}
 
 #test:
 fsim.1 <- fsim.2 <- fsim.3<- list()
+n.sim=10
 num_cl = 5
 nloop = 30
-for(i in 1:10) {
+for(i in 1:n.sim) {
   fsim.1 <- append(fsim.1,list(iter_cluster(covars,make_cl1(num_cl),X,n.loop=nloop)))
   fsim.2 <- append(fsim.2,list(iter_cluster(covars,make_cl2(num_cl),X,n.loop=nloop)))
   fsim.3 <- append(fsim.3,list(iter_cluster(covars,make_cl3(num_cl),X,n.loop=nloop)))
@@ -46,11 +47,18 @@ save.image()
 naive.dev <- multi.devian(X,as.matrix(covars),coef(fits))
 ftotres<-list(fsim.1,fsim.2,fsim.3)
 #prepare results matrix:
-resm <- array(dim=c(3,1))
+resm <- as.data.frame(c(rep(1,10),rep(2,10),rep(3,10))
+
+resm$deviance = 0
 
 for (i in 1:3) {
-rel_sim <- ftotres[[i]][[1]]
-resm[i] <- multi.devian(X,as.matrix(cbind(covars,model.matrix(~0+as.factor(rel_sim$clusters)))),rel_sim$B)
+for(j in 1:n.sim) {
+print(i)
+print(j)
+rel_sim <- totres[[i]][[1]]
+resm$deviance[(i-1)*10+j] <- multi.devian(X,as.matrix(cbind(covars,model.matrix(~0+as.factor(rel_sim$clusters)))),rel_sim$B)
+resm$elapsed[(i-1)*10+j]<- rel_sim$time[3]
+}
 }
 
 rel_sim<-sim.1[[1]]
@@ -63,13 +71,29 @@ par(mfrow=c(3,4))
 titles =c("Random Initialization", "K means Initialization", "Residual K Means")
 nclust <- c("5","10")
 likes <- array(dim=c(15,12))
-for (i in 1:3) {
-    plot(ftotres[[i]]$full_likes,type="l",ylab="relevant log likelihood",xlab="Iterations",main=paste(titles[i]," \n # clusters = ",nclust[(j>2)+1]))
+
+plot(ftotres[[3]][[1]]$likes$cluster_likes,type="l",ylab="relevant log likelihood",xlab="Iterations",main="Residual K means Initialization")
+for (i in 2:n.sim) {
+    lines(ftotres[[3]][[i]]$likes$cluster_likes,type="l",col=i)
 }
 
+
+plot(resm$init,resm$deviance,ylim=c(-1350000,-1000000),main="Multinomial Deviance",xlab="Initialization Type \n (1=rand, 2=kmeans, 3= resid kmeans)")
+abline(naive.dev,0)
+	
 #########Let's investigate the clustering results..? 
 final_clusters <- cbind(cl2,ftotres[[1]][[1]]$h.clusters[,10],ftotres[[1]][[2]]$h.clusters[,15],
                         ftotres[[2]][[1]]$h.clusters[,10],ftotres[[2]][[2]]$h.clusters[,15])
 
 table(final_clusters[final_clusters[,2]==1,3])
 
+collect_results<-as.data.frame(rep(1:30,30))
+collect_results$init = c(rep(1,300),rep(2,300),rep(3,300))
+collect_results$likes = 0
+for(i in 1:3) { for (j in 1:10) {
+collect_results$likes[((i-1)*300+(j-1)*30+1):((i-1)*300+j*30)] = ftotres[[i]][[j]]$likes$cluster_likes
+}}
+library(ggplot2)
+p<-ggplot(collect_results,aes(x=Iteration,y=likes,colour=init))
+p <- p + layer(geom = "point") +ggtitle("Likelihood convergence of clusters")+scale_fill_discrete(name="Cluster Initialization")
+p  #render plot
