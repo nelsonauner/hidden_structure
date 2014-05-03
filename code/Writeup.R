f7219e97f77ad98cefa9785ae195bb2ed3b0b7d0
=Nelson Auner <ne.auner@gmail.com>
2014-05-03 11:17:06 -0500

    good results

--- code/Writeup.R
+++ code/Writeup.R
@@ -36,6 +36,7 @@ table(cong_res[[1]][[1]]$h.clusters[,14]) #no longer evenly distributed
 length(beta[4,][beta[4,]!=0])  #the first cluster
 #find highest rated coeffecients for each cluster and puht into a table!
 
+
 head(beta[6,][order(abs(beta[6,]),decreasing=TRUE)])
 berta
 high_loadings <- function(beta,cl_range,digits=2) {
@@ -51,7 +52,23 @@ highloadings <- data.frame(do.call(cbind,lapply(cl_index,
 names(highloadings) = rep(c("term","loading"),length(cl_range))
 return(highloadings)
 }
+}
 
-y<-high_loadings(beta,c(6,8))
-z
+(y<-high_loadings(beta,c(6,8)))
 
+
+##Sweep out statistics from our 15 simulations: 
+sink("congress_topic.txt")
+cat("Referenced by initialization method, then by # of clusters")
+hl_list = list()
+num_cl_vec = c(5,10,15,20,25)
+for (method in 1:3) {
+for (cls in 1:5) {
+cat(method);cat(num_cl_vec[cls]);cat("\n")
+print(high_loadings(
+										cong_res[[method]][[cls]]$B,
+										(4:(3+num_cl_vec[cls]))))
+
+	}
+	}
+sink()
